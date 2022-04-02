8cb32059573c26ff5a09c19329aaf3cfdafdf650
Rendy Vickriansyah <64136018+Rendyvickriansyah@users.noreply.github.com>
2022-04-02 10:57:16 +0700

    test lagi

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("https://ngivent-api.herokuapp.com/"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", ["APP_KEYS1", "APP_KEYS2"]),
   },
 });
